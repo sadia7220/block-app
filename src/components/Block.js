67d26babefb1a24cf03de4645c72bef465471d38
sadia <sadiaalam1007@gmail.com>
2022-10-25 17:49:49 +0600

    app js file, array format, icon setting and code indentation are modified

---
+++ src/components/Block.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+
+export default function Block(props) {
+
+  const clickToRightHandler = (val) => {
+    let index = 0;
+    for (index; index < props.block_array.length; index++) {
+      if (props.block_array[index] === val) {
+        break;
+      }
+    }
+    if (index === 0 || index < props.block_array.length - 1) {
+      const copyArray = [...props.block_array];
+      [copyArray[index], copyArray[index + 1]] = [copyArray[index + 1], copyArray[index]];
+      props.setBlock_array(copyArray);
+    }
+  }
+
+  const clickToLeftHandler = (val) => {
+    let index = 0;
+    for (index; index < props.block_array.length; index++) {
+      if (props.block_array[index] === val) {
+        break;
+      }
+    }
+    if (index > 0 || index === props.block_array.length - 1) {
+      const copyArray = [...props.block_array];
+      [copyArray[index - 1], copyArray[index]] = [copyArray[index], copyArray[index - 1]];
+      props.setBlock_array(copyArray);
+    }
+  }
+
+  return (
+    <div className="App">
+      {
+        props.block_array?.map((element, i) => (
+          <div className={`block${element}`} key={element}>
+            <h1>{element}</h1>
+            {
+              i === 0 ?
+                <AiFillCaretRight onClick={() => clickToRightHandler(element)} className='rightIcon' />
+                : i === (props.block_array.length - 1) ?
+                  <AiFillCaretLeft onClick={() => clickToLeftHandler(element)} className='leftIcon' />
+                  :
+                  <>
+                    <AiFillCaretLeft onClick={() => clickToLeftHandler(element)} className='leftIcon' />
+                    <AiFillCaretRight onClick={() => clickToRightHandler(element)} className='rightIcon' />
+                  </>
+            }
+          </div>
+        ))
+      }
+    </div>
+  );
+}
